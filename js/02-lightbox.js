fb4d5564e10ffb02145ecb1894cbbfda872630e2
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2021-12-23 01:07:06 +0200

    07-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkUp = galleryItems.map(({preview, original, description})=> {
+  return `<a
+      class="gallery__link"
+      href="${original}"      
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"       
+        alt="${description}"
+      />
+   </a>`;
+}).join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkUp);
+
+galleryEl.addEventListener("click", openModal);
+
+function openModal(e) {  
+  e.preventDefault();
+    let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+}
